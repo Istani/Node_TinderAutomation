c68c6855c6bfc9905d0175f42daf3930a147b4af
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2020-02-05 15:43:10 +0100

    Change Docker to Detach and Restart Always

--- _start.sh
+++ _start.sh
@@ -1,3 +1,3 @@
 docker build -t node_puppeteer .
 
-docker run -ti --shm-size=1gb --rm --name tinder_automation -e DISPLAY=192.168.178.58:0.0 -v //c/Dropbox/SimpleSoftwareStudioShare/Node_TinderAutomation/:/app node_puppeteer
+docker run -dit --shm-size=1gb --name tinder_automation --restart always -v //c/Dropbox/SimpleSoftwareStudioShare/Node_TinderAutomation/:/app node_puppeteer
