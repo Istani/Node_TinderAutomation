ca21671a8bced34754694ccd4c15b72f8cb45065
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2020-02-06 11:00:22 +0100

    Better Dokcer Build with .dockerignore

--- _start.sh
+++ _start.sh
@@ -1,3 +1,2 @@
 docker build -t node_puppeteer .
-
-docker run -dit --shm-size=1gb --name tinder_automation --restart always -v //c/Dropbox/SimpleSoftwareStudioShare/Node_TinderAutomation/:/app node_puppeteer
+#docker run -dit --shm-size=1gb --name tinder_automation --restart always -v //c/Dropbox/SimpleSoftwareStudioShare/Node_TinderAutomation/:/app node_puppeteer
